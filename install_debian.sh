38e7939296fbc4a184f7ebc85adfc4e275df9905
Troy Rippin <troy@mac-mini.thenoisykeyboard.com>
2026-01-24 06:55:01 +0100

    update debian install script

--- install_debian.sh
+++ install_debian.sh
@@ -121,7 +121,7 @@ install_starship() {
     sudo apt-get install -y -qq starship > /dev/null 2>&1
     cp dotfiles/starship/starship.toml $HOME/.config
     sed -i -e 's/A3BE8C/BF616A/g' $HOME/.config/starship.toml
-    #sed -i -e 's/A3BE8C/D08770/g' $HOME/.config/starship.toml
+    #sed -i -e 's/BF616A/D08770/g' $HOME/.config/starship.toml
     whiptail --title "Install completed" --msgbox "Starship installed and configuration restored." 10 80
 }
 
