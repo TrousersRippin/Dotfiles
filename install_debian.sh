4967ef11d0beaece081e0618a03c5635ae4850e0
Troy Rippin <troy_rippin@icloud.com>
2026-04-11 17:22:48 +0200

    updates

--- install_debian.sh
+++ install_debian.sh
@@ -27,24 +27,24 @@ echo -e "${yellow}[ INFO ]${reset} It will create .cache, .config and .local fol
 
 # Check Git installed
 function check_git() {
-    if command -v git >/dev/null 2>&1; then
-        echo -e "${yellow}[ INFO ]${reset} Git already installed."
-    else
-        sudo apt-get install -y -qq git > /dev/null 2>&1
-        echo -e "${green}[ NOTICE ]${reset} Git has been installed."
-    fi
+  if command -v git >/dev/null 2>&1; then
+    echo -e "${yellow}[ INFO ]${reset} Git already installed."
+  else
+    sudo apt-get install -y -qq git > /dev/null 2>&1
+    echo -e "${green}[ NOTICE ]${reset} Git has been installed."
+  fi
 }
 
 check_git
 
 # Check Whiptail installed
 function check_whiptail() {
-    if command -v whiptail >/dev/null 2>&1; then
-        echo -e "${yellow}[ INFO ]${reset} Whiptail already installed."
-    else
-        sudo apt-get install -y -qq whiptail > /dev/null 2>&1
-        echo -e "${green}[ NOTICE ]${reset} Whiptail has been installed."
-    fi
+  if command -v whiptail >/dev/null 2>&1; then
+    echo -e "${yellow}[ INFO ]${reset} Whiptail already installed."
+  else
+    sudo apt-get install -y -qq whiptail > /dev/null 2>&1
+    echo -e "${green}[ NOTICE ]${reset} Whiptail has been installed."
+  fi
 }
 
 check_whiptail
@@ -53,144 +53,144 @@ mkdir -p $HOME/.cache $HOME/.config $HOME/.local/share $HOME/.local/state
 
 # Software menu
 CHOICES=( $(whiptail --separate-output --title "Linux software & dotfiles installer" --backtitle "<Tab> moves; <Space> select; <Enter> continue;" --checklist \
-    "Select packages to install" 0 80 10 \
-    "bat" "text viewer" ON \
-    "btop" "system monitor" ON \
-    "curl" "network downloader" ON \
-    "duf" "disk usage viewer" ON \
-    "fastfetch" "system information" ON \
-    "gnupg" "encryption software" ON \
-    "htop" "system monitor" ON \
-    "less" "terminal pager" ON \
-    "starship" "terminal prompt" ON \
-    "vim" "code editor" ON \
-    "wget" "network downloader" ON \
-    "zsh" "modern shell" ON \
-    3>&1 1>&2 2>&3) )
+  "Select packages to install" 0 80 10 \
+  "bat" "text viewer" ON \
+  "btop" "system monitor" ON \
+  "curl" "network downloader" ON \
+  "duf" "disk usage viewer" ON \
+  "fastfetch" "system information" ON \
+  "gnupg" "encryption software" ON \
+  "htop" "system monitor" ON \
+  "less" "terminal pager" ON \
+  "starship" "terminal prompt" ON \
+  "vim" "code editor" ON \
+  "wget" "network downloader" ON \
+  "zsh" "modern shell" ON \
+  3>&1 1>&2 2>&3) )
 
 install_bat() {
-    sudo apt-get install -y -qq bat > /dev/null 2>&1
-    cp -R dotfiles/bat $HOME/.config
-    whiptail --title "Install completed" --msgbox "Bat installed and configuration restored." 10 80
+  sudo apt-get install -y -qq bat > /dev/null 2>&1
+  cp -R dotfiles/bat $HOME/.config
+  whiptail --title "Install completed" --msgbox "Bat installed and configuration restored." 10 80
 }
 
 install_btop() {
-    sudo apt-get install -y -qq btop > /dev/null 2>&1
-    cp -R dotfiles/btop $HOME/.config
-    whiptail --title "Install completed" --msgbox "Btop installed and configuration restored." 10 80
+  sudo apt-get install -y -qq btop > /dev/null 2>&1
+  cp -R dotfiles/btop $HOME/.config
+  whiptail --title "Install completed" --msgbox "Btop installed and configuration restored." 10 80
 }
 
 install_curl() {
-    sudo apt-get install -y -qq curl > /dev/null 2>&1
-    whiptail --title "Install completed" --msgbox "Curl installed." 10 80
+  sudo apt-get install -y -qq curl > /dev/null 2>&1
+  whiptail --title "Install completed" --msgbox "Curl installed." 10 80
 }
 
 install_duf() {
-    sudo apt-get install -y -qq duf > /dev/null 2>&1
-    cp -R dotfiles/duf $HOME/.config
-    whiptail --title "Install completed" --msgbox "Duf installed and configuration restored." 10 80
+  sudo apt-get install -y -qq duf > /dev/null 2>&1
+  cp -R dotfiles/duf $HOME/.config
+  whiptail --title "Install completed" --msgbox "Duf installed and configuration restored." 10 80
 }
 
 install_fastfetch() {
-    sudo apt-get install -y -qq fastfetch > /dev/null 2>&1
-    cp -R dotfiles/fastfetch $HOME/.config
-    whiptail --title "Install completed" --msgbox "Fastfetch installed and configuration restored." 10 80
+  sudo apt-get install -y -qq fastfetch > /dev/null 2>&1
+  cp -R dotfiles/fastfetch $HOME/.config
+  whiptail --title "Install completed" --msgbox "Fastfetch installed and configuration restored." 10 80
 }
 
 install_gnupg() {
-    sudo apt-get install -y -qq gpg > /dev/null 2>&1
-    mkdir -p $HOME/.local/share/gnupg
-    touch $HOME/.local/share/gnupg/gpg-agent.service
-    echo '[Service]
-    Environment="GNUPGHOME=%h/.local/share/gnupg"' >> $HOME/.local/share/gnupg/gpg-agent.service
-    whiptail --title "Install completed" --msgbox "Gnupg installed and configuration restored." 10 80
+  sudo apt-get install -y -qq gpg > /dev/null 2>&1
+  mkdir -p $HOME/.local/share/gnupg
+  touch $HOME/.local/share/gnupg/gpg-agent.service
+  echo '[Service]
+  Environment="GNUPGHOME=%h/.local/share/gnupg"' >> $HOME/.local/share/gnupg/gpg-agent.service
+  whiptail --title "Install completed" --msgbox "Gnupg installed and configuration restored." 10 80
 }
 
 install_htop() {
-    sudo apt-get install -y -qq htop > /dev/null 2>&1
-    cp -R dotfiles/htop $HOME/.config
-    whiptail --title "Install completed" --msgbox "Htop installed and configuration restored." 10 80
+  sudo apt-get install -y -qq htop > /dev/null 2>&1
+  cp -R dotfiles/htop $HOME/.config
+  whiptail --title "Install completed" --msgbox "Htop installed and configuration restored." 10 80
 }
 
 install_less() {
-    sudo apt-get install -y -qq less > /dev/null 2>&1
-    cp dotfiles/less/lesskey $HOME/.config
-    whiptail --title "Install completed" --msgbox "Less installed and configuration restored." 10 80
+  sudo apt-get install -y -qq less > /dev/null 2>&1
+  cp dotfiles/less/lesskey $HOME/.config
+  whiptail --title "Install completed" --msgbox "Less installed and configuration restored." 10 80
 }
 
 install_starship() {
-    sudo apt-get install -y -qq starship > /dev/null 2>&1
-    cp dotfiles/starship/starship.toml $HOME/.config
-    if grep -qi "ubuntu" /etc/os-release; then
-        sed -i -e 's/7A9A63/D08770/g' $HOME/.config/starship.toml
-    else
-        sed -i -e 's/7A9A63/BF616A/g' $HOME/.config/starship.toml
-    fi
-    whiptail --title "Install completed" --msgbox "Starship installed and configuration restored." 10 80
+  sudo apt-get install -y -qq starship > /dev/null 2>&1
+  cp dotfiles/starship/starship.toml $HOME/.config
+  if grep -qi "ubuntu" /etc/os-release; then
+    sed -i -e 's/7A9A63/D08770/g' $HOME/.config/starship.toml
+  else
+    sed -i -e 's/7A9A63/BF616A/g' $HOME/.config/starship.toml
+  fi
+  whiptail --title "Install completed" --msgbox "Starship installed and configuration restored." 10 80
 }
 
 install_vim() {
-    sudo apt-get install -y -qq vim > /dev/null 2>&1
-    cp -R dotfiles/vim $HOME/.config &&
-    mkdir -p $HOME/.config/vim/after $HOME/.config/vim/autoload $HOME/.config/vim/pack/{hashivim,itchyny,junegunn,ojroques,preservim,ryanoasis,tpope}/{start,opt} > /dev/null 2>&1
-    git clone --quiet https://github.com/hashivim/vim-terraform.git $HOME/.config/vim/pack/hashivim/start/vim-terraform > /dev/null 2>&1
-    git clone --quiet https://github.com/itchyny/lightline.vim      $HOME/.config/vim/pack/itchyny/start/lightline > /dev/null 2>&1
-    git clone --quiet https://github.com/junegunn/fzf.git           $HOME/.config/vim/pack/junegunn/start/fzf > /dev/null 2>&1
-    git clone --quiet https://github.com/junegunn/fzf.vim.git       $HOME/.config/vim/pack/junegunn/start/fzf.vim > /dev/null 2>&1
-    git clone --quiet https://github.com/ojroques/vim-oscyank.git   $HOME/.config/vim/pack/ojroques/start/vim-oscyank > /dev/null 2>&1
-    git clone --quiet https://github.com/preservim/nerdtree.git     $HOME/.config/vim/pack/preservim/start/nerdtree > /dev/null 2>&1
-    git clone --quiet https://github.com/ryanoasis/vim-devicons     $HOME/.config/vim/pack/ryanoasis/start/vim-devicons > /dev/null 2>&1
-    git clone --quiet https://github.com/tpope/vim-sensible.git     $HOME/.config/vim/pack/tpope/start/vim-sensible > /dev/null 2>&1
-    whiptail --title "Install completed" --msgbox "Vim installed and configuration restored." 10 80
+  sudo apt-get install -y -qq vim > /dev/null 2>&1
+  cp -R dotfiles/vim $HOME/.config &&
+  mkdir -p $HOME/.config/vim/after $HOME/.config/vim/autoload $HOME/.config/vim/pack/{hashivim,itchyny,junegunn,ojroques,preservim,ryanoasis,tpope}/{start,opt} > /dev/null 2>&1
+  git clone --quiet https://github.com/hashivim/vim-terraform.git $HOME/.config/vim/pack/hashivim/start/vim-terraform > /dev/null 2>&1
+  git clone --quiet https://github.com/itchyny/lightline.vim    $HOME/.config/vim/pack/itchyny/start/lightline > /dev/null 2>&1
+  git clone --quiet https://github.com/junegunn/fzf.git       $HOME/.config/vim/pack/junegunn/start/fzf > /dev/null 2>&1
+  git clone --quiet https://github.com/junegunn/fzf.vim.git     $HOME/.config/vim/pack/junegunn/start/fzf.vim > /dev/null 2>&1
+  git clone --quiet https://github.com/ojroques/vim-oscyank.git   $HOME/.config/vim/pack/ojroques/start/vim-oscyank > /dev/null 2>&1
+  git clone --quiet https://github.com/preservim/nerdtree.git   $HOME/.config/vim/pack/preservim/start/nerdtree > /dev/null 2>&1
+  git clone --quiet https://github.com/ryanoasis/vim-devicons   $HOME/.config/vim/pack/ryanoasis/start/vim-devicons > /dev/null 2>&1
+  git clone --quiet https://github.com/tpope/vim-sensible.git   $HOME/.config/vim/pack/tpope/start/vim-sensible > /dev/null 2>&1
+  whiptail --title "Install completed" --msgbox "Vim installed and configuration restored." 10 80
 }
 
 install_wget() {
-    sudo apt-get install -y -qq wget > /dev/null 2>&1
-    whiptail --title "Install completed" --msgbox "Wget installed." 10 80
+  sudo apt-get install -y -qq wget > /dev/null 2>&1
+  whiptail --title "Install completed" --msgbox "Wget installed." 10 80
 }
 
 install_zsh() {
-    mkdir -p $HOME/.cache/zsh $HOME/.local/state/zsh
-    sudo apt-get install -y -qq zsh > /dev/null 2>&1
-    cp dotfiles/dir_colors/dir_colors $HOME/.config
-    cp -R dotfiles/zsh $HOME/.config
-    git clone --quiet https://github.com/zsh-users/zsh-autosuggestions.git      $HOME/.config/zsh/plugins/zsh-autosuggestions > /dev/null 2>&1
-    git clone --quiet https://github.com/zsh-users/zsh-completions.git          $HOME/.config/zsh/plugins/zsh-completions > /dev/null 2>&1
-    git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting.git  $HOME/.config/zsh/plugins/zsh-syntax-highlighting > /dev/null 2>&1
-    ln -s $HOME/.config/zsh/.zshenv $HOME/.zshenv
-    rm $HOME/.config/zsh/.zprofile
-    sudo chsh -s $(which zsh) $USER > /dev/null 2>&1
-    whiptail --title "Install completed" --msgbox "ZSH installed and configuration restored." 10 80
+  mkdir -p $HOME/.cache/zsh $HOME/.local/state/zsh
+  sudo apt-get install -y -qq zsh > /dev/null 2>&1
+  cp dotfiles/dir_colors/dir_colors $HOME/.config
+  cp -R dotfiles/zsh $HOME/.config
+  git clone --quiet https://github.com/zsh-users/zsh-autosuggestions.git    $HOME/.config/zsh/plugins/zsh-autosuggestions > /dev/null 2>&1
+  git clone --quiet https://github.com/zsh-users/zsh-completions.git      $HOME/.config/zsh/plugins/zsh-completions > /dev/null 2>&1
+  git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting.git  $HOME/.config/zsh/plugins/zsh-syntax-highlighting > /dev/null 2>&1
+  ln -s $HOME/.config/zsh/.zshenv $HOME/.zshenv
+  rm $HOME/.config/zsh/.zprofile
+  sudo chsh -s $(which zsh) $USER > /dev/null 2>&1
+  whiptail --title "Install completed" --msgbox "ZSH installed and configuration restored." 10 80
 }
 
 for choice in "${CHOICES[@]}"; do
-    case $choice in
-        bat)            install_bat ;;
-        btop)           install_btop ;;
-        curl)           install_curl ;;
-        duf)            install_duf ;;
-        fastfetch)      install_fastfetch ;;
-        git)            install_git ;;
-        gnupg)          install_gnupg ;;
-        htop)           install_htop ;;
-        less)           install_less ;;
-        starship)       install_starship ;;
-        vim)            install_vim ;;
-        wget)           install_wget ;;
-        zsh)            install_zsh ;;
-        *)              exit ;;
-    esac
+  case $choice in
+    bat)      install_bat ;;
+    btop)       install_btop ;;
+    curl)       install_curl ;;
+    duf)      install_duf ;;
+    fastfetch)    install_fastfetch ;;
+    git)      install_git ;;
+    gnupg)      install_gnupg ;;
+    htop)       install_htop ;;
+    less)       install_less ;;
+    starship)     install_starship ;;
+    vim)      install_vim ;;
+    wget)       install_wget ;;
+    zsh)      install_zsh ;;
+    *)        exit ;;
+  esac
 done
 
 # Post-Install
 function post_install() {
-    if whiptail --title "Post Install" --yesno "Do you want to run user post install?" 10 80; then
-        rm $HOME/.bash_logout $HOME/.bash_profile $HOME/.bashrc $HOME/.profile $HOME/.zcompdump> /dev/null 2>&1
-        touch $HOME/.hushlogin
-        echo -e "${red}[ NOTICE ]${reset} Post install completed."
-    else
-        echo -e "${yellow}[ INFO ]${reset} Post install not completed."
-    fi
+  if whiptail --title "Post Install" --yesno "Do you want to run user post install?" 10 80; then
+    rm $HOME/.bash_logout $HOME/.bash_profile $HOME/.bashrc $HOME/.profile $HOME/.zcompdump> /dev/null 2>&1
+    touch $HOME/.hushlogin
+    echo -e "${red}[ NOTICE ]${reset} Post install completed."
+  else
+    echo -e "${yellow}[ INFO ]${reset} Post install not completed."
+  fi
 }
 
 post_install
@@ -199,11 +199,11 @@ rm -fr dotfiles
 
 # Exit/Reboot
 function exit_reboot() {
-    if whiptail --title "Exit or Reboot"  --yes-button "Exit" --no-button "Reboot" --yesno "Do you want to exit or reboot?" 10 80; then
-        exit
-    else
-        sudo reboot
-    fi
+  if whiptail --title "Exit or Reboot"  --yes-button "Exit" --no-button "Reboot" --yesno "Do you want to exit or reboot?" 10 80; then
+    exit
+  else
+    sudo reboot
+  fi
 }
 
 exit_reboot
